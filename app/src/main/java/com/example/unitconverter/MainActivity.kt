1f3caf7f5e78b6ce43dc3243c611103190060a0e
Darrell <67837432+phantomcodingllc@users.noreply.github.com>
2025-03-18 11:57:39 -0400

    UI update
    Updated UI, need to finish certain UI functionality.

--- app/src/main/java/com/example/unitconverter/MainActivity.kt
+++ app/src/main/java/com/example/unitconverter/MainActivity.kt
@@ -4,18 +4,31 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.unitconverter.ui.theme.UnitConverterTheme
 
 class MainActivity : ComponentActivity() {
@@ -37,24 +50,67 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun UnitConverter(){
-    Column {
+    Column (Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally){
         // Elements will be stacked below each other
         
         Text("Unit Converter")
+        Spacer(modifier = Modifier.height(16.dp))
         OutlinedTextField(value = "Enter value", onValueChange ={
             // This is what should happen
             // when the value of OutlinedTextField changes
         })
-
+        Spacer(modifier = Modifier.height(16.dp))
         Row {
+        Box{
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = "Select")
+                Icon(Icons.Default.ArrowDropDown, contentDescription = "Arrow Down")
+            }
+            DropdownMenu(expanded = false,
+                onDismissRequest = {  }) {
+                DropdownMenuItem(text = { Text("Millimeters") },
+                    onClick = { /*TODO*/ })
 
-            val context = LocalContext.current
-            // Elements will be stacked next to each other
-            Button(onClick = { Toast.makeText(context, "Thanks for clicking!",
-                Toast.LENGTH_LONG).show() }) {
-                Text("Click to convert")
+                DropdownMenuItem(text = { Text("Centimeters") },
+                    onClick = { /*TODO*/ })
+
+                DropdownMenuItem(text = { Text("Meters") },
+                    onClick = { /*TODO*/ })
+
+                DropdownMenuItem(text = { Text("Feet") },
+                    onClick = { /*TODO*/ })
+
+
             }
         }
+            Spacer(modifier = Modifier.width(16.dp))
+        Box{
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = "Select")
+                Icon(Icons.Default.ArrowDropDown, contentDescription = "Arrow Down")
+            }
+            DropdownMenu(expanded = false,
+                onDismissRequest = {  }) {
+                DropdownMenuItem(text = { Text("Millimeters") },
+                onClick = { /*TODO*/ })
+
+                DropdownMenuItem(text = { Text("Centimeters") },
+                    onClick = { /*TODO*/ })
+
+                DropdownMenuItem(text = { Text("Meters") },
+                    onClick = { /*TODO*/ })
+
+                DropdownMenuItem(text = { Text("Feet") },
+                    onClick = { /*TODO*/ })
+
+
+            }
+        }
+
+        }
+        Spacer(modifier = Modifier.height(16.dp))
         Text("Result:")
     }
 }
